3bdf6daf3a4b57e6c6a73ff5b8b33238fd747cf2
mancity Vic <victory20031@outlook.com>
2024-05-25 16:11:28 -0500

    Python code

---
+++ band-Name-Generator.py
@@ -0,0 +1,7 @@
+#Day 1 band Name Generator
+print("Welcome to the Band Name Generator.")
+city_Name = input("What's the name of the city you grew up in?\n")
+
+pet_Name = input("What's your pet's name?\n")
+
+print("Your band name could be " + city_Name + " " + pet_Name)
