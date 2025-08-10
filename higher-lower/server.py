b653c304d4808f6b8b5b2dbf80b8b9627b9b82ac
Yeyonritu <victory20031@outlook.com>
2025-08-10 03:07:42 -0500

    Higher-Lower Redo

--- higher-lower/server.py
+++ higher-lower/server.py
@@ -1,30 +1,28 @@
-import random
 from flask import Flask
+import random
 
 app = Flask(__name__)
+rand_num = random.randint(0, 10)
 
-rand_num = random.randint(0, 9)
-# rand_num = 5 For testing purposes
+@app.route('/')
+def home():
+    return '<h1>Guess a number between 0 and 9</h1>' \
+            '<img src = https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExN2RpbTg0M2IyN3d0YnNpaWg4ZzE1eWNxeW4yMHJqeWU4ZGVjbHdzMiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/PINwITJa3v5RJimYHA/giphy.gif>'
 
-@app.route("/")
-def home_screen():
-    return '<h1>Guess a Number between 0 and 9</h1>' \
-           '<img src = "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExeWFseHlqdmtmaGl3YmZqZzM2bmo0Njl2MzZyeXoyYTMwZmcxNzRwbCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/26uflDxU6cEhrhmUg/200.webp">'
-
-@app.route("/URL/<int:number>")
-def guess_game(number):
-    
+@app.route('/<int:number>')
+def rand(number):
     if number < rand_num:
-        return '<h1 style="color:red;">Too low, Try again!</h1>' \
-                '<img src="https://media0.giphy.com/media/2ALdBWhJpGcodDssuR/giphy.webp?cid=790b76110f3ygtqx8g4bgm2xm3sbmncbliww09e3w6k31jhe&ep=v1_gifs_search&rid=giphy.webp&ct=g">'
+        return '<h1 style = "color: red">Too low! Try Again</h1>' \
+                '<img src = https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExNm5sYXE0Y3N1cTl1eDR3bTR2NjAwbWJmc2xjaXc0MnV3ejFueThkYyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/s1MX7LfOn5nhTyb0dy/giphy.gif>'
     
     elif number > rand_num:
-        return '<h1 style="color:purple;">Too high, Try again!</h1>'\
-                '<img src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNHc0MTFxbDVvd2t1aTRmcDcxaWR0NnEzYjMxNmdleG5uMjQ3NXdzeiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/0aQJL0nm32LTIXus5F/giphy.webp">'
-                
+        return '<h1 style = "color: red">Too high! Try Again</h1>' \
+                '<img src = https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExY3A1bG5hc2RzZmdmNXp3eW9iNmY4NWZiNnlub21pMHYwb29wcnRjdiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/2cei8MJiL2OWga5XoC/giphy.gif>'
+
     else:
-        return '<h1 style="color:green;">You found me!</h1>'\
-               '<img src = "https://media0.giphy.com/media/cruO3FTeoAxjiTVxPW/200.webp?cid=790b76114rnicpig0y9sdefxva13r2ohv601ogx6vi7cguyb&ep=v1_gifs_search&rid=200.webp&ct=g">'
+        return '<h1 style = "color: green">Exactly Right</h1>' \
+                '<img src = https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExeDcwdnMwYmp4cW9nNXV5YW80eXVleXppcmhyOXVleThpOTNrZTNwZCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/UAXK9VGoJTbdcPgmcJ/giphy.gif>'     
 
 if __name__ == "__main__":
     app.run(debug=True)
+
